6edb7ff0e1e38e256987d6e4ae650bc3bf5cd7f0
Espen Meidell <espen.meidell@gmail.com>
2016-04-11 18:07:45 +0200

    Tror det meste funker

--- app/src/main/java/mmi_prototype/app/Me.java
+++ app/src/main/java/mmi_prototype/app/Me.java
@@ -1,25 +1,77 @@
 package mmi_prototype.app;
 
+import android.app.Dialog;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Me extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
 
+        Button vennerbtn = (Button) findViewById(R.id.minevenner);
+        vennerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Me.this, Venner.class);
+                Me.this.startActivity(myIntent);
+            }
+        });
+
+
+        Button trofebtn = (Button) findViewById(R.id.trofebtn);
+        trofebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImage();
+            }
+        });
+
 
 
     }
+
+    public void showImage() {
+        final Dialog builder = new Dialog(this);
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                builder.dismiss();
+            }
+        });
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageURI(Uri.parse("android.resource://mmi_prototype.app/" + R.drawable.trofe));
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.dismiss();
+            }
+        });
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
